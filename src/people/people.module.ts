ecff16f2786f9ca7cca147e0c51effaf8dcb701d
Stephen Hanjaya <stephenhanjaya@gmail.com>
2023-04-30 16:41:50 -0700

    update people.module

--- src/people/people.module.ts
+++ src/people/people.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PeopleController } from './people.controller';
 import { PeopleService } from './people.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Person } from 'src/entity/person.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Person])],
   controllers: [PeopleController],
   providers: [PeopleService],
 })
