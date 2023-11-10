d5fe0758b8fc98fd659d31610af8de273a765005
Stephen Hanjaya <stephenhanjaya@gmail.com>
2023-11-10 11:45:47 -0800

    temp update on people module for typeORM=>sequelize

--- src/people/people.module.ts
+++ src/people/people.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PeopleController } from './people.controller';
 import { PeopleService } from './people.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { Person } from 'src/entity/person.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Person])], // this enable to injectRepository in service
+  imports: [],
   controllers: [PeopleController],
   providers: [PeopleService],
-  exports: [TypeOrmModule], // needed if we want to use the repo outside of this module
+  exports: [],
 })
 export class PeopleModule {}
