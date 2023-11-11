e57b06bf2ccd9c390c9cd0a790fe626ff1f76f09
Stephen Hanjaya <stephenhanjaya@gmail.com>
2023-11-10 16:37:00 -0800

    update .eslintrc,.prettierrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,21 +20,34 @@ module.exports = {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off'
+    '@typescript-eslint/no-explicit-any': 'off',
   },
   overrides: [
     {
-      files: ["*.ts"],
+      files: ['*.ts'],
       rules: {
         'space-before-function-paren': [
           'error',
           {
-            anonymous: 'always',
-            named: 'always',
-            asyncArrow: 'always'
-          }
-        ]
-      }
-    }
-  ]
+            anonymous: 'never',
+            named: 'never',
+            asyncArrow: 'always',
+          },
+        ],
+        'comma-dangle': [
+          'error',
+          {
+            arrays: 'always-multiline',
+            objects: 'always-multiline',
+            imports: 'always-multiline',
+            exports: 'always-multiline',
+            functions: 'only-multiline',
+          },
+        ],
+        'array-bracket-spacing': ['error', 'never'],
+        'object-curly-spacing': ['error', 'always'],
+        'computed-property-spacing': ['error', 'always'],
+      },
+    },
+  ],
 };
