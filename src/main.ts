8195ef5bad029ef6f75446b270e7db74a9eca96c
Stephen Hanjaya <stephenhanjaya@gmail.com>
2023-11-19 18:31:35 -0800

    clean up main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // global endpoints prefix
   app.setGlobalPrefix('api/v1');
+
   await app.listen(3000);
 }
 bootstrap();
