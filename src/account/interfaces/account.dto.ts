89ce6c304a4a75bcdf015e2a1f414d575ec6c305
Stephen Hanjaya <stephenhanjaya@gmail.com>
2023-11-20 21:51:45 -0800

    refactor account.dto

--- src/account/interfaces/account.dto.ts
+++ src/account/interfaces/account.dto.ts
@@ -1,22 +1,8 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { IsEmail, IsNotEmpty, Min } from 'class-validator';
+
 import { AccountRole } from '../../core/db/models/account.model';
 import { AccountEntity } from '../account.entity';
-
-import { IsEmail, IsNotEmpty, Min, Contains } from 'class-validator';
-
-export class AccountDto {
-  readonly id: number;
-  readonly username: string;
-  readonly password: string;
-  readonly role: AccountRole;
-}
-
-export class FindAccount {
-  where: {
-    id?: number;
-    email?: string;
-    username?: string;
-  };
-}
 
 export class RegisterAccount {
   id?: number;
@@ -33,4 +19,33 @@ export class RegisterAccount {
   @IsNotEmpty()
   @Min(8)
   password: string;
+
+  confirmPassword: string;
 }
+
+export class LoginDTO {
+  readonly username: string;
+  readonly email: string;
+  readonly password: string;
+}
+
+export class RegisterDTO extends LoginDTO {
+  readonly confirmPassword: string;
+}
+
+export class AccountDTO extends PartialType(LoginDTO) {
+  readonly id: number;
+  readonly role: AccountRole;
+}
+
+export class QueryAccount {
+  where: {
+    id?: number;
+    email?: string;
+    username?: string;
+  };
+}
+
+export class PasswordlessAccountEntity extends PartialType(AccountEntity) {}
+export class PartialAccountDTO extends PartialType(AccountDTO) {}
+export class PartialLoginDTO extends PartialType(LoginDTO) {}
