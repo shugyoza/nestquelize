f3819fc38495ed55ee7af027faf5fecc5237dff1
Stephen Hanjaya <stephenhanjaya@gmail.com>
2023-11-20 21:54:08 -0800

    delete accounts-http.module

--- src/account/accounts.module.ts
+++ src/account/accounts.module.ts
@@ -2,12 +2,10 @@ import { Module } from '@nestjs/common';
 
 import { DbModule } from '../core/db/db.module';
 import { AccountsService } from './accounts.service';
-import { AccountsController } from './accounts.controller';
 import { accountsProviders } from './accounts.providers';
 
 @Module({
   imports: [DbModule],
   providers: [AccountsService, ...accountsProviders],
-  controllers: [AccountsController],
 })
 export class AccountsModule {}
